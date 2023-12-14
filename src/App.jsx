b97990a30ba1498c1f02b0ac5c96f6ad5af40541
Talita Aguiar <87323173+aguiartalita@users.noreply.github.com>
2023-12-13 21:01:15 -0300

    Modulo 03 do curso
    Incluindo interação ,adicao e exclusao de comentario.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,80 @@
 // JSX = Javascript + XML
-import { Post } from "./Post"
-import { Header } from "./components/Header"
-import { SideBar } from './components/Sidebar'
-import styles from './App.module.css'
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { SideBar } from "./components/Sidebar";
 
-import "./global.css"
+import styles from "./App.module.css";
 
+import "./global.css";
+
+// Será necessario incluir interação nos seguintes elementos dos componentes:
+// author: { avatar:url: "", name: "", role: ""}
+//publishedAt:Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        "https://plus.unsplash.com/premium_photo-1664867432842-16ae7a30aabd?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVzc29hJTIwbmVncmF8ZW58MHx8MHx8fDA%3D",
+      name: " Raissa Barbosa ",
+      role: "Web Developer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: " Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime reiciendis optio doloremque?",
+      },
+    ],
+    publishedAt: new Date("2023-12-12 16:02:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:
+        "https://plus.unsplash.com/premium_photo-1661255424747-6ee7fd8d86f6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJsYWNrJTIwcGVvcGxlJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: " Bruna Dias Goncalves ",
+      role: " Designer UI/UX",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: " Fala pessoal 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻 ",
+      },
+
+      {
+        type: "link",
+        content: "devonlane.design",
+      },
+      {
+        type: "paragraph",
+        content:
+          "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime reiciendis optio doloremque?",
+      },
+    ],
+    publishedAt: new Date("2023-12-13 12:58:00"),
+  },
+];
 
 export function App() {
   return (
@@ -14,19 +83,18 @@ export function App() {
       <div className={styles.wrapper}>
         <SideBar />
         <main>
-          <Post
-            author="João Maria"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, odio unde, maxime nihil sit,perspiciatis officiis laboriosam dicta hic exercitationem laudantium aliquam perferendis ad placeat esse adipisci vitae architecto voluptatem?"
-          />
-          <Post
-            author="Maria João"
-            content=" Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid veritatis voluptas dolores sunt, iste itaque!"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
-
     </div>
-  )
+  );
 }
-
-
---
+++ src/components/Comment.jsx
@@ -0,0 +1,45 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+
+export function Comment({ content, deleteComment }) {
+  function handleDeleteComment() {
+    deleteComment(content);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fFBFU1NPQSUyME5FR1JBJTIwQVZBVEFSfGVufDB8fDB8fHww"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Julia Rocha</strong>
+              <time
+                dateTime="2023-12-12 16:02:00"
+                title="12 de dezembro as 16:02h"
+              >
+                Cerca de 1h atras
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar comentario">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span> 28</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
